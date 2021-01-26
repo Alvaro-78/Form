34b11943d1c20d1d4910401b9ad8779938890a1d
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-01-26 17:41:31 +0100

    Add Javascript to get data from inputs

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert(rellena todos los campos)
+const recogerDatos = () => {
+
+  let usuarioCliente = document.getElementById("usuario_cliente").value
+  let passwordCliente = document.getElementById("password_cliente").value
+
+  console.log("usuario", usuarioCliente)
+  console.log("password", passwordCliente)
+}
+
